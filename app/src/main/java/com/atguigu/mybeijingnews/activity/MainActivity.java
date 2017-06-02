abf125cf2452801d74218be047e4893dbe287d8e
rx1996 <3343108599@qq.com>
2017-06-02 10:51:02 +0800

    欢迎界面完成及跳转引导界面

---
+++ app/src/main/java/com/atguigu/mybeijingnews/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.mybeijingnews.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.atguigu.mybeijingnews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
