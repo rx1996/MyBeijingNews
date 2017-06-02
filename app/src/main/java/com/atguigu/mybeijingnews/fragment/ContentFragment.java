8fb0d7b5037caaffc6aef121148fb48198dcb46f
rx1996 <3343108599@qq.com>
2017-06-02 16:34:30 +0800

    七 :    主页面正文Fragment的布局

--- app/src/main/java/com/atguigu/mybeijingnews/fragment/ContentFragment.java
+++ app/src/main/java/com/atguigu/mybeijingnews/fragment/ContentFragment.java
@@ -1,29 +1,48 @@
 package com.atguigu.mybeijingnews.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.atguigu.mybeijingnews.R;
 import com.atguigu.mybeijingnews.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Administrator on 2017/6/2.
  */
 
 public class ContentFragment extends BaseFragment {
+    @InjectView(R.id.vp)
+    ViewPager vp;
+    @InjectView(R.id.rg_main)
+    RadioGroup rgMain;
     private TextView textView;
+
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_content, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
+
     @Override
     public void initData() {
         super.initData();
-        textView.setText("主页面");
+        //默认选中主页
+        rgMain.check(R.id.rb_home);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
