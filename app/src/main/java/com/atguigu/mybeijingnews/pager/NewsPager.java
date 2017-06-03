875178b9592346dd5e7e3cbe230f920d74a13fd9
rx1996 <3343108599@qq.com>
2017-06-03 10:54:25 +0800

    十三 :   新闻中心数据请求&json数据解析

--- app/src/main/java/com/atguigu/mybeijingnews/pager/NewsPager.java
+++ app/src/main/java/com/atguigu/mybeijingnews/pager/NewsPager.java
@@ -2,10 +2,18 @@ package com.atguigu.mybeijingnews.pager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
 import com.atguigu.mybeijingnews.base.BasePager;
+import com.atguigu.mybeijingnews.domain.NewsCenterBean;
+import com.atguigu.mybeijingnews.utils.ConstantUtils;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  * Created by Administrator on 2017/6/2.
@@ -32,5 +40,36 @@ public class NewsPager extends BasePager {
 
         //添加到布局上
         fl_content.addView(textView);
+        //联网请求
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        //新闻中心的网络路径
+        String url = ConstantUtils.NEWSCENTER_PAGER_URL;
+        OkHttpUtils
+                .get()
+                .url(url)
+//                .addParams("username", "hyman")
+//                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "请求失败==" + e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "请求成功==" + response);
+                        processData(response);
+                    }
+                });
+    }
+    private void processData(String json) {
+        NewsCenterBean newsCenterBean = new Gson().fromJson(json,NewsCenterBean.class);
+        Log.e("TAG","解析成功了哦=="+ newsCenterBean.getData().get(0).getChildren().get(0).getTitle());
+
     }
 }
