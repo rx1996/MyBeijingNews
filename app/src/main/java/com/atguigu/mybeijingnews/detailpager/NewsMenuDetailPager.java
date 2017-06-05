0e759871f73bb8e73b6828894b1766797be7aec3
rx1996 <3343108599@qq.com>
2017-06-05 10:04:35 +0800

    十九 :   新闻详情页面(NewsMenuDetailPager)的填充

--- app/src/main/java/com/atguigu/mybeijingnews/detailpager/NewsMenuDetailPager.java
+++ app/src/main/java/com/atguigu/mybeijingnews/detailpager/NewsMenuDetailPager.java
@@ -2,35 +2,83 @@ package com.atguigu.mybeijingnews.detailpager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.atguigu.mybeijingnews.R;
 import com.atguigu.mybeijingnews.base.MenuDetailBasePager;
+import com.atguigu.mybeijingnews.domain.NewsCenterBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/3.
  */
 
 public class NewsMenuDetailPager extends MenuDetailBasePager {
-    private TextView textView;
-    public NewsMenuDetailPager(Context context) {
-        super(context);
-    }
+    //TabDetailPager的对应的数据
+    private final List<NewsCenterBean.DataBean.ChildrenBean> datas;
+    private ViewPager viewpager;
+    //TabDetailPager页面集合
+    private List<TabDetailPager> tabDetailPagers;
 
+    public NewsMenuDetailPager(Context context, List<NewsCenterBean.DataBean.ChildrenBean> children) {
+        super(context);
+        this.datas = children;
+    }
 
     @Override
     public View initView() {
+        View view = View.inflate(context, R.layout.pager_news_menu_detail,null);
+        viewpager = (ViewPager) view.findViewById(R.id.viewpager);
         //创建子类的视图
-        textView = new TextView(context);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("新闻详情页面的内容");
+
+        //根据数据创建子页面
+        tabDetailPagers = new ArrayList<>();
+        for(int i = 0; i < datas.size(); i++) {
+            //一会再传递数据
+            tabDetailPagers.add(new TabDetailPager(context,datas.get(i)));
+        }
+
+        //设置适配器
+        viewpager.setAdapter(new NewsMenuDetailPagerAdapter());
+    }
+
+    class NewsMenuDetailPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return tabDetailPagers == null ? 0 : tabDetailPagers.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view ==object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TabDetailPager tabDetailPager = tabDetailPagers.get(position);
+            View rootView = tabDetailPager.rootView;
+            container.addView(rootView);
+            tabDetailPager.initData();
+            return rootView;
+        }
     }
 }
